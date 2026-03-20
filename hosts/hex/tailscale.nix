dd2e5915d664beb9762f85cffb4cc691c1a509ee
Patrick Podbregar <patrick.podbregar@proton.me>
2026-03-20 22:03:43 +0100

    feat: create a service to autoconnect to tailscale

--- hosts/hex/tailscale.nix
+++ hosts/hex/tailscale.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   services.tailscale.enable = true;
 
@@ -9,4 +9,40 @@
   # Standard Tailscale setup often requires allowing the
   # virtual network interface
   networking.firewall.checkReversePath = "loose";
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+    after = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wants = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      # Wait for the tailscale socket to be ready
+      sleep 2
+
+      #Check current status
+      status=$(${pkgs.tailscale}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)
+      if [ "$status" = "Running" ]; then
+        exit 0
+      fi
+
+      # Read the key from the sops secret path
+      AUTH_KEY=$(cat ${config.sops.secrets.tailscaleAuthKey.path})
+
+      # Run tailscale up
+      ${pkgs.tailscale}/bin/tailscale up --authkey "$AUTH_KEY"
+    '';
+  };
+
+  environment.systemPackages = [
+    pkgs.jq
+    pkgs.tailscale
+  ];
 }
