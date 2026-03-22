c5879781f01ea58ba148d0a472a23539b601900c
Patrick Podbregar <patrick.podbregar@proton.me>
2026-03-22 13:56:53 +0100

    fix: use stdenv.hostPlatform.system instead of system

--- home/patrick/common/shell/terminals.nix
+++ home/patrick/common/shell/terminals.nix
@@ -2,7 +2,7 @@
 {
   programs.ghostty = {
     enable = true;
-    package = inputs.ghostty.packages.${pkgs.system}.default;
+    package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
     settings = {
       "background-opacity" = 0.9;
