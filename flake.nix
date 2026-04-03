14870013a0342562df38777e10501d160310dd46
Patrick Podbregar <patrick.podbregar@proton.me>
2026-04-03 11:30:14 +0200

    feat: add ghostty wrapper module

--- flake.nix
+++ flake.nix
@@ -6,8 +6,16 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
+    # nix-wrapper-modules
+    wrappers = {
+      url = "github:BirdeeHub/nix-wrapper-modules";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # NixOS hardware support
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    # Ghostty official flake
+    ghostty.url = "github:ghostty-org/ghostty";
   };
 
   outputs =
