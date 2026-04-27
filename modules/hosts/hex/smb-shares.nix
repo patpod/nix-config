a45655ef8789bc9e7862ab6b18de9a0dbab76ac5
Patrick Podbregar <patrick.podbregar@proton.me>
2026-04-27 21:32:44 +0200

    feat: add spogf smb share

--- modules/hosts/hex/smb-shares.nix
+++ modules/hosts/hex/smb-shares.nix
@@ -29,5 +29,26 @@
           "credentials=${config.sops.secrets."truenas-credentials".path}"
         ];
       };
+
+      fileSystems."/home/patrick/spogf" = {
+        device = "//truenas.bouncyhaus.com/spogf";
+        fsType = "cifs";
+        options = [
+          # Standard systemd automount options for laptops
+          "x-systemd.automount"
+          "noauto"
+          "x-systemd.idle-timeout=60"
+          "x-systemd.device-timeout=5s"
+          "x-systemd.mount-timeout=5s"
+
+          # Make sure you can read/write to it as your normal user
+          "uid=1000"
+          "gid=100"
+
+          # This is the magic line!
+          # It dynamically injects the path to the decrypted secret.
+          "credentials=${config.sops.secrets."truenas-credentials".path}"
+        ];
+      };
     };
 }
